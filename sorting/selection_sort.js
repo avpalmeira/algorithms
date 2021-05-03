1fce6936bf4a97b7fcf40e2e5b0034af8be93447
Antonio Palmeira <av.palmeira@gmail.com>
2021-05-03 17:42:34 -0300

    simplify selection sort algorithm

--- sorting/selection_sort.js
+++ sorting/selection_sort.js
@@ -4,22 +4,26 @@ function sort(arrayToBeSorted) {
   const array = arrayToBeSorted;
   // For each N position of the new array, select the Nth minimum value to fullfil it
   for (let n = 0; n < array.length; n++) {
-    let currentMinimum = array[n];
+    let currentMinimumIndex = n;
     // Iterate over the array to be sorted looking for its minimum value
     for (let comparingIndex = n + 1; comparingIndex < array.length; comparingIndex++) {
-      // If a new minimum value is found...
-      if (array[comparingIndex] < currentMinimum) {
-        let newMinimum = array[comparingIndex];
-        // Set last minimum to the position where is the new minimum
-        array[comparingIndex] = currentMinimum;
-        // And set new minimum as the current minimum value
-        currentMinimum = newMinimum;
+      // If a new minimum value is found save its index
+      if (array[comparingIndex] < array[currentMinimumIndex]) {
+        currentMinimumIndex = comparingIndex;
       }
     }
-    array[n] = currentMinimum;
+    // After all values in the array are compared...
+    // Swap Nth value and Minimum Value in the array
+    swapValues(n, currentMinimumIndex, array);
   }
 
   return array;
+}
+
+function swapValues(positionA, positionB, array) {
+  let temp = array[positionA];
+  array[positionA] = array[positionB];
+  array[positionB] = temp;
 }
 
 // Using a set as intermediate data structure
