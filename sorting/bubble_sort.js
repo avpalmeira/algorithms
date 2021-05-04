457e1aba51c11e9b12c9506d7ce9d681d5a98015
Antonio Palmeira <av.palmeira@gmail.com>
2021-05-04 11:42:22 -0300

    refactor(bubble_sort): swapValues in a separated function

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -3,16 +3,15 @@ function sort(arrayToBeSorted) {
   const array = [...arrayToBeSorted];
   let nextBubbledItemIndex = array.length - 1;
 
-  // Iterate over all itens in the array comparing them by pairs, while lastBubbledItemIndex != 0;
+  // While not all items in the array are bubbled...
   while (nextBubbledItemIndex > 0) {
-    // We can optimize sorting speed by checking that array is sorted after comparing all items in the array
+    // (We can optimize sorting speed by checking that array is already sorted)
     let isSorted = true;
+    // Iterate over all items in the array comparing them by pairs 
     for (let comparingIndex = 0; comparingIndex < nextBubbledItemIndex; comparingIndex++) {
       // If item at the left position is bigger than item at the right position, then swap items
       if (array[comparingIndex] > array[comparingIndex + 1]) {
-        let temporaryRegistry = array[comparingIndex];
-        array[comparingIndex] = array[comparingIndex + 1];
-        array[comparingIndex + 1] = temporaryRegistry;
+        swapValues(comparingIndex, comparingIndex + 1, array);
         // If we have to swap any items in the array, then the array is not sorted
         isSorted = false;
       }
@@ -22,6 +21,12 @@ function sort(arrayToBeSorted) {
   }
 
   return array;
+}
+
+function swapValues(positionA, positionB, array) {
+  let temp = array[positionA];
+  array[positionA] = array[positionB];
+  array[positionB] = temp;
 }
 
 const arrayToBeSorted = [8, 2, 4, 9, 1, 3, 9, 6, 5, 5, 4, 1, 8, 6, 7];
